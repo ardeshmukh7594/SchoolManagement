398048ab40cb62c2d434a44cb813aecbc849f536
Akshay <ardeshmukh7594@gmail.com>
2025-07-10 20:21:55 +0530

    deleted

--- SchoolManagement/src/main/java/com/example/demo/repository/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Teacher;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
-
-}
